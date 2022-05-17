db30b3aa7b84c3f7754d81eb3488c4310456cfb9
Mateusz Majewski <mateuszmjwsk@gmail.com>
2022-05-17 12:50:17 +0200

    Initial Commit

---
+++ src/main/java/com/example/SalePlatform/SalePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.SalePlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalePlatformApplication.class, args);
+	}
+
+}
